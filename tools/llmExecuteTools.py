39c71621136112c3447b40352fb070b0b7a27452
liuwenqiang <wenqiang.liu@wenge.com>
2025-04-21 18:01:25 +0800

    rag检索

--- tools/llmExecuteTools.py
+++ tools/llmExecuteTools.py
@@ -12,6 +12,7 @@ llm = ChatOllama(
 )
 
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+wikipedia.invoke("")
 
 @tool
 def add(a: int, b: int) -> int:
@@ -37,7 +38,7 @@ tools = [wikipedia,add,subtract,multiply,divide]
 map_of_tools = {tool.name:tool for tool in tools}
 
 # 初始问题
-query = "In which year did Messi win the World Cup championship"
+query = "In which year did Messi win the World Cup championship？"
 message = [HumanMessage(query)]
 
 # 构建绑定tools的模型对象，把tools都传进去
@@ -48,10 +49,14 @@ ai_message = llm_bind_tools.invoke(message)
 # 初始问题也就是人的问题，然后把ai的答案拼起来，此时得到了两个信息
 message.append(ai_message)
 
+# 遍历ai的执行结果，取出他选择的tool
 for tool_call in ai_message.tool_calls:
+    # 统一小写
     tool_name = tool_call['name'].lower()
+    # 从map映射取出对应的tool
     execute_tool = map_of_tools[tool_name]
     # 执行tool，并且把tool_call传入，其实这个tool_call是个map结构，正是参数
+    print(tool_call)
     tool_invoke = execute_tool.invoke(tool_call)
     message.extend(tool_invoke)
 
